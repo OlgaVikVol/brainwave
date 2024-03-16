fbffdcedf9c33c7f50693ea072c48b564e74fc54
Olga Volobueva <olvik1605@gmail.com>
2024-03-15 20:22:58 -0400

    add Collaboration

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import ButtonGradient from "./assets/svg/ButtonGradient";
 import Button from "./components/Button";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
 
 const App = () => {
   return (
@@ -10,6 +12,8 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
+        <Benefits />
+        <Collaboration />
       </div>
       <ButtonGradient />
     </>
